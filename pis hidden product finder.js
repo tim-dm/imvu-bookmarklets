9d1462a63e50b6eac8d1f06e4c2a36dfde17f020
tim-dm <imvu_data@zoho.com>
2022-04-22 10:12:12 -0700

    Update pis hidden product finder.js

--- pis hidden product finder.js
+++ pis hidden product finder.js
@@ -3,49 +3,74 @@ let url = window.location.href;
 if(url.includes("products_in_scene.php")) {
 	//replace the url encoding
 	url = url.replaceAll("%3B", ";");
-	
-	const roomPids = getRoomPids(url);	
-	
-	//grab all of the product links on the page
-	const links = document.querySelectorAll(".thumbnail-link");
-	let hiddenPids = [];
 
-	if(links.length > 0) {
-		links.forEach((link) => {
-			//strip array the irrelevant data leaving the pid behind
-			let pid = link.href.split("products_id=")[1];
-			
-			if(pid) {
-				//if the pid exists in the url
-				if(roomPids.includes(pid)) {
-					//change the opacity of the link to indicate we don't care about it
-					link.style.opacity = .5;
-				} else {
-					hiddenPids.push(pid);
-				}
+	const roomPids = getPidsInUrl(url);
+
+	let visiblePids = getPidsDisplayedOnPage();
+	let hiddenPids = [];
+	
+	if(visiblePids.length > 0) {
+		roomPids.forEach((pid) => {
+			if(!visiblePids.includes(pid)) {
+				hiddenPids.push(pid);
 			}
 		});
 	}
 	
-	console.log(hiddenPids);
+	displayPids(hiddenPids);
 }
 
+function displayPids(pids) {
+	let output = "";
+
+	if(pids.length) {
+		output += "Hidden Pids:";
+		output += "\n";
+		output += pids.join("\r\n");
+	} else {
+		output += "No hidden pids found";
+	}
+	
+	alert(output);
+}
 
 /**
- * Returns an array of pids.
+ * Returns an array of pids from the links on the page.
  *
  * @param {string} pisUrl The products in scene url.
  * @return {array} pids formatted.
  */
-function getRoomPids(pisUrl) {
+function getPidsDisplayedOnPage() {
+	let links = document.querySelectorAll(".thumbnail-link");	
+	let visiblePids = [];
+	
+	if(links.length > 0) {
+		links.forEach((link) => {
+			//strip array the irrelevant data leaving the pid behind
+			let pid = link.href.split("products_id=")[1];
+
+			if(pid) {
+				visiblePids.push(pid);
+			}
+		});
+	}
+	
+	return visiblePids;
+}
+
+/**
+ * Returns an array of pids from the url.
+ *
+ * @param {string} pisUrl The products in scene url.
+ * @return {array} pids formatted.
+ */
+function getPidsInUrl(pisUrl) {
 	let pids = pisUrl.split('?')[1];
 
 	if (pids.startsWith("room=")) {
 		pids = pisUrl.split('&avatar')[0];
 		pids = pids.replace("https://www.imvu.com/catalog/products_in_scene.php?room=", "");
 		pids = pids.split(';');
-		return pids;
-
 	} else if (pids.startsWith("avatar")) {
 		let urlParts = pids.split('&');
 		for (i = 0; i < urlParts.length; ++i) {
@@ -56,8 +81,9 @@ function getRoomPids(pisUrl) {
 		}
 		pids = pids.replace("room=", "");
 		pids = pids.split(';');
-		return cleanPids(pids);
 	}
+
+	return cleanPids(pids);
 }
 
 /**
@@ -66,11 +92,11 @@ function getRoomPids(pisUrl) {
  * @param {array} pids The products in scene url.
  * @return {array} pids without the x counter.
  */
-function cleanPids(pids) {
+function cleanPids(dirtyPids) {
 	let buffer = [];
 
-	for (i = 0; i < pids.length; ++i) {
-		let cleanPid = pids[i];
+	for (i = 0; i < dirtyPids.length; ++i) {
+		let cleanPid = dirtyPids[i];
 		if(cleanPid.includes("x")) {
 			cleanPid = cleanPid.split('x')[0];
 		}
